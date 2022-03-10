3370b2a53b235ba93498789b1682867f95b293b2
Wenping Guo <ybyygu@gmail.com>
2022-03-10 14:50:37 +0800

    interactive: interaction with child process's stdio

--- src/lib.rs
+++ src/lib.rs
@@ -13,20 +13,19 @@ fn timestamp_now() -> String {
 // 16bab924 ends here
 
 // [[file:../runners.note::9fd14bf8][9fd14bf8]]
-mod session;
-// 9fd14bf8 ends here
-
-// [[file:../runners.note::d8e4f605][d8e4f605]]
-/// Some extension traits
-pub mod prelude {
-    pub use crate::process::SpawnSessionExt;
-}
-
 pub mod cli;
 pub mod job;
 pub mod process;
 pub mod stop;
-// d8e4f605 ends here
+pub mod interactive;
+
+mod session;
+
+/// Some extension traits
+pub mod prelude {
+    pub use crate::process::SpawnSessionExt;
+}
+// 9fd14bf8 ends here
 
 // [[file:../runners.note::c6e9d2bf][c6e9d2bf]]
 #[cfg(feature = "adhoc")]
